f8075ecea183159f55ec07f022b450c855342ce3
Code Foxx <iamcodefoxx@gmail.com>
2022-03-01 10:17:13 -0800

    update

--- mail.js
+++ mail.js
@@ -18,7 +18,8 @@ resetPasswordEmail = (email, id, token) => {
       from: "iamcodefoxx@gmail.com",
       to: email,
       subject: "Password reset",
-      html: `<p>You are receiving this because you (or someone else) have requested the reset of the password for your account. Please click on the following link, or paste it into your browser to complete the process within one hour of receiving it:<br><br><a href="http://localhost:3000/password-reset-update/${id}${token}">http://localhost:3000/password-reset-update/${id}${token}</a><br><br>If you did not request this, please ignore this email and your password will remain unchanged.<br><br>The Team</p>`
+      // html: `<p>You are receiving this because you (or someone else) have requested the reset of the password for your account. Please click on the following link, or paste it into your browser to complete the process within one hour of receiving it:<br><br><a href="http://localhost:3000/password-reset-update/${id}${token}">http://localhost:3000/password-reset-update/${id}${token}</a><br><br>If you did not request this, please ignore this email and your password will remain unchanged.<br><br>The Team</p>`
+      html: `<p>You are receiving this because you (or someone else) have requested the reset of the password for your account. Please click on the following link, or paste it into your browser to complete the process within one hour of receiving it:<br><br><a href="https://minu-login-system.herokuapp.com/password-reset-update/${id}${token}">https://minu-login-system.herokuapp.com/password-reset-update/${id}${token}</a><br><br>If you did not request this, please ignore this email and your password will remain unchanged.<br><br>The Team</p>`
   };
 
   transporter.sendMail(mailOptions, function(error, info) {
@@ -36,7 +37,8 @@ activateAccountEmail = (email, id, token) => {
       from: "iamcodefoxx@gmail.com",
       to: email,
       subject: "Account activation",
-      html: `<p>Please click on the following link, or paste it into your browser to complete the account activation process:<br><br><a href="http://localhost:3000/account-verification-success/${id}${token}">http://localhost:3000/account-verification-success/${id}${token}</a><br><br>The Team</p>`
+      // html: `<p>Please click on the following link, or paste it into your browser to complete the account activation process:<br><br><a href="http://localhost:3000/account-verification-success/${id}${token}">http://localhost:3000/account-verification-success/${id}${token}</a><br><br>The Team</p>`
+      html: `<p>Please click on the following link, or paste it into your browser to complete the account activation process:<br><br><a href="https://minu-login-system.herokuapp.com/account-verification-success/${id}${token}">https://minu-login-system.herokuapp.com/account-verification-success/${id}${token}</a><br><br>The Team</p>`
   };
 
   transporter.sendMail(mailOptions, function(error, info) {
